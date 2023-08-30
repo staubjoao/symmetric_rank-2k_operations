b1b8cb99c8191bd8b36ac2d0ca9d11bc50ee1b4f
yonix42 <joao.staub42@gmail.com>
2023-08-30 14:41:51 -0300

    testando coisas

--- teste.c
+++ teste.c
@@ -1,167 +1,111 @@
-#include <stdlib.h>
+#include <mpi.h>
 #include <stdio.h>
-#include "mpi.h"
-#include <time.h>
-#include <sys/time.h>
 
-/*
-  In here 'process o' which distribute the workload to other processes is considered
-  as Root (Master) process and other processes which do the computation is considered
-  as Slave task processes.
-*/
+#define SIZE 8 /* Size of matrices */
 
-// Number of rows and columnns in a matrix
-#define N 4
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
 
-MPI_Status status;
+// mpic++ teste.c -o teste.exe
+// mpiexec -n 4 teste.exe
 
-// Matrix holders are created
-double matrix_a[N][N], matrix_b[N][N], matrix_c[N][N];
-
-int main(int argc, char **argv)
+void fill_matrix(int m[SIZE][SIZE])
 {
-    int processCount, processId, slaveTaskCount, source, dest, rows, offset;
+    static int n = 0;
+    int i, j;
+    for (i = 0; i < SIZE; i++)
+        for (j = 0; j < SIZE; j++)
+            m[i][j] = n++;
+}
 
-    struct timeval start, stop;
-
-    // MPI environment is initialized
-    MPI_Init(&argc, &argv);
-    // Each process gets unique ID (rank)
-    MPI_Comm_rank(MPI_COMM_WORLD, &processId);
-    // Number of processes in communicator will be assigned to variable -> processCount
-    MPI_Comm_size(MPI_COMM_WORLD, &processCount);
-
-    // Number of slave tasks will be assigned to variable -> slaveTaskCount
-    slaveTaskCount = processCount - 1;
-
-    // Root (Master) process
-    if (processId == 0)
+void print_matrix(int m[SIZE][SIZE])
+{
+    int i, j = 0;
+    for (i = 0; i < SIZE; i++)
     {
+        printf("\n\t| ");
+        for (j = 0; j < SIZE; j++)
+            printf("%5d ", m[i][j]);
+        printf("|");
+    }
+}
 
-        // Matrix A and Matrix B both will be filled with random numbers
-        srand(time(NULL));
-        for (int i = 0; i < N; i++)
+int main(int argc, char *argv[])
+{
+    int myrank, P, from, to, i, j, k;
+    int tag = 666; /* any value will do */
+    MPI_Status status;
+    int tamanho_matriz;
+
+    for (int i = 0; i < argc; i++)
+    {
+        if (!strcmp(argv[i], "-d"))
         {
-            for (int j = 0; j < N; j++)
-            {
-                matrix_a[i][j] = rand() % 10;
-                matrix_b[i][j] = rand() % 10;
-            }
+            if (strcmp(argv[i + 1], "small") == 0)
+                // tamanho_matriz = 1600;
+                tamanho_matriz = 3200;
+            else if (strcmp(argv[i + 1], "medium") == 0)
+                // tamanho_matriz = 2400;
+                tamanho_matriz = 4000;
+            else if (strcmp(argv[i + 1], "large") == 0)
+                // tamanho_matriz = 3200;
+                tamanho_matriz = 4800;
+            i++;
         }
-
-        printf("\n\t\tMatrix - Matrix Multiplication using MPI\n");
-
-        // Print Matrix A
-        printf("\nMatrix A\n\n");
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                printf("%.0f\t", matrix_a[i][j]);
-            }
-            printf("\n");
-        }
-
-        // Print Matrix B
-        printf("\nMatrix B\n\n");
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
-                printf("%.0f\t", matrix_b[i][j]);
-            }
-            printf("\n");
-        }
-
-        // Determine number of rows of the Matrix A, that is sent to each slave process
-        rows = N / slaveTaskCount;
-        // Offset variable determines the starting point of the row which sent to slave process
-        offset = 0;
-
-        // Calculation details are assigned to slave tasks. Process 1 onwards;
-        // Each message's tag is 1
-        for (dest = 1; dest <= slaveTaskCount; dest++)
-        {
-            // Acknowledging the offset of the Matrix A
-            MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-            // Acknowledging the number of rows
-            MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-            // Send rows of the Matrix A which will be assigned to slave process to compute
-            MPI_Send(&matrix_a[offset][0], rows * N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-            // Matrix B is sent
-            MPI_Send(&matrix_b, N * N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-
-            // Offset is modified according to number of rows sent to each process
-            offset = offset + rows;
-        }
-
-        // Root process waits untill the each slave proces sent their calculated result with message tag 2
-        for (int i = 1; i <= slaveTaskCount; i++)
-        {
-            source = i;
-            // Receive the offset of particular slave process
-            MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-            // Receive the number of rows that each slave process processed
-            MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-            // Calculated rows of the each process will be stored int Matrix C according to their offset and
-            // the processed number of rows
-            MPI_Recv(&matrix_c[offset][0], rows * N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
-        }
-
-        // Print the result matrix
-        printf("\nResult Matrix C = Matrix A * Matrix B:\n\n");
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-                printf("%.0f\t", matrix_c[i][j]);
-            printf("\n");
-        }
-        printf("\n");
     }
 
-    // Slave Processes
-    if (processId > 0)
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank); /* who am i */
+    MPI_Comm_size(MPI_COMM_WORLD, &P);      /* number of processors */
+
+    /* Just to use the simple variants of MPI_Gather and MPI_Scatter we */
+    /* impose that SIZE is divisible by P. By using the vector versions, */
+    /* (MPI_Gatherv and MPI_Scatterv) it is easy to drop this restriction. */
+
+    if (SIZE % P != 0)
     {
+        if (myrank == 0)
+            printf("Matrix size not divisible by number of processors\n");
+        MPI_Finalize();
+        exit(-1);
+    }
 
-        // Source process ID is defined
-        source = 0;
+    from = myrank * SIZE / P;
+    to = (myrank + 1) * SIZE / P;
 
-        // Slave process waits for the message buffers with tag 1, that Root process sent
-        // Each process will receive and execute this separately on their processes
+    /* Process 0 fills the input matrices and broadcasts them to the rest */
+    /* (actually, only the relevant stripe of A is sent to each process) */
 
-        // The slave process receives the offset value sent by root process
-        MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-        // The slave process receives number of rows sent by root process
-        MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-        // The slave process receives the sub portion of the Matrix A which assigned by Root
-        MPI_Recv(&matrix_a, rows * N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
-        // The slave process receives the Matrix B
-        MPI_Recv(&matrix_b, N * N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+    if (myrank == 0)
+    {
+        fill_matrix(A);
+        fill_matrix(B);
+    }
 
-        // Matrix multiplication
+    MPI_Bcast(B, SIZE * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(A, SIZE * SIZE / P, MPI_INT, A[from], SIZE * SIZE / P, MPI_INT, 0, MPI_COMM_WORLD);
 
-        for (int k = 0; k < N; k++)
+    printf("computing slice %d (from row %d to %d)\n", myrank, from, to - 1);
+    for (i = from; i < to; i++)
+        for (j = 0; j < SIZE; j++)
         {
-            for (int i = 0; i < rows; i++)
-            {
-                // Set initial value of the row summataion
-                matrix_c[i][k] = 0.0;
-                // Matrix A's element(i, j) will be multiplied with Matrix B's element(j, k)
-                for (int j = 0; j < N; j++)
-                    matrix_c[i][k] = matrix_c[i][k] + matrix_a[i][j] * matrix_b[j][k];
-            }
+            C[i][j] = 0;
+            for (k = 0; k < SIZE; k++)
+                C[i][j] += A[i][k] * B[k][j];
         }
 
-        // Calculated result will be sent back to Root process (process 0) with message tag 2
+    MPI_Gather(C[from], SIZE * SIZE / P, MPI_INT, C, SIZE * SIZE / P, MPI_INT, 0, MPI_COMM_WORLD);
 
-        // Offset will be sent to Root, which determines the starting point of the calculated
-        // value in matrix C
-        MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-        // Number of rows the process calculated will be sent to root process
-        MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-        // Resulting matrix with calculated rows will be sent to root process
-        MPI_Send(&matrix_c, rows * N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+    if (myrank == 0)
+    {
+        printf("\n\n");
+        print_matrix(A);
+        printf("\n\n\t       * \n");
+        print_matrix(B);
+        printf("\n\n\t       = \n");
+        print_matrix(C);
+        printf("\n\n");
     }
 
     MPI_Finalize();
+    return 0;
 }
